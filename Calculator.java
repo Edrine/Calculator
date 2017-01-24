bd9265161a41dee19c5cda0cecade8bdb69d1306
Edrine <edrine.ssemwanga@ashesi.edu.gh>
2017-01-24 21:38:57 +0100

    This is the updated version of the calculator

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,21 @@
 public class Calculator {
   public static void main(String [] parameters ) {
     if(parameters[0].equals("add")){
-            double num1 = Double.parseDouble(parameters[1]);
-            double num2 = Double.parseDouble(parameters[2]);
-            System.out.println(num1+num2);
-        }
-     if(parameters[0].equals("subtract")){
-            double num1 = Double.parseDouble(parameters[1]);
-            double num2 = Double.parseDouble(parameters[2]);
-            System.out.println(num1-num2);
-        }
+      double num1 = Double.parseDouble(parameters[1]);
+      double num2 = Double.parseDouble(parameters[2]);
+      System.out.println(num1+num2);
+    }
+   else if(parameters[0].equals("subtract")){
+      double num1 = Double.parseDouble(parameters[1]);
+      double num2 = Double.parseDouble(parameters[2]);
+      System.out.println(num1-num2);
+    }
+ else if(parameters[0].equals("Multiply")){
+      double num1 = Double.parseDouble(parameters[1]);
+      double num2 = Double.parseDouble(parameters[2]);
+      System.out.println(num1*num2);
+    }
+    
   }
   
 }
