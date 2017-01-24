43431bfbaf3541a8a8dd6372cfac6c33a6c429a6
Umutari <umutariy@gmail.com>
2017-01-24 21:51:55 +0000

    added divide

--- Calculator.java
+++ Calculator.java
@@ -5,17 +5,21 @@ public class Calculator {
       double num2 = Double.parseDouble(parameters[2]);
       System.out.println(num1+num2);
     }
-   else if(parameters[0].equals("subtract")){
+    else if(parameters[0].equals("subtract")){
       double num1 = Double.parseDouble(parameters[1]);
       double num2 = Double.parseDouble(parameters[2]);
       System.out.println(num1-num2);
     }
- else if(parameters[0].equals("Multiply")){
+    else if(parameters[0].equals("Multiply")){
       double num1 = Double.parseDouble(parameters[1]);
       double num2 = Double.parseDouble(parameters[2]);
       System.out.println(num1*num2);
     }
-    
+    else if(parameters[0].equals("Divide")){
+      double num1 = Double.parseDouble(parameters[1]);
+      double num2 = Double.parseDouble(parameters[2]);
+      System.out.println(num1/num2);
+    }
   }
   
 }
